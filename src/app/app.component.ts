54b0d571d777f04bba7b5b48f1f75b789169c64f
kunal-thakkar <kunalmthakkar@gmail.com>
2021-10-21 09:36:24 +0530

    Completed MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireStorage, AngularFireStorageReference } from '@angular/fire/storage';
+import firebase from 'firebase/app';
+import * as CryptoJS from 'crypto-js';
+import { HttpClient } from '@angular/common/http';
+import { StringFormat } from '@angular/fire/storage/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'locker';
+export class AppComponent implements OnInit {
+
+  ref: AngularFireStorageReference;
+  info: string;
+  path: string;
+  key: string;
+  data: StringFormat;
+
+  constructor(public auth: AngularFireAuth, private storage: AngularFireStorage, private httpClient: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.auth.user.subscribe(user => {
+      if (user != null) {
+        this.path = 'data/' + user.uid + '/default';
+        this.ref = this.storage.ref(this.path);
+      } else {
+        this.info = '';
+        this.path = '';
+      }
+    })
+  }
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.auth.signOut();
+  }
+
+  load() {
+    if (this.key === '') {
+      alert("Please enter valid PIN");
+      return;
+    }
+    this.ref.getDownloadURL().toPromise().then(url => {
+      this.httpClient.get(url, { responseType: 'text' }).subscribe((d: string) => {
+        this.info = CryptoJS.AES.decrypt(d, this.key.trim()).toString(CryptoJS.enc.Utf8);
+      })
+    }).catch(e => console.log(e))
+  }
+
+  save() {
+    if (!this.key) {
+      alert("Please enter valid PIN");
+      return;
+    }
+    this.storage.upload(this.path, new Blob([CryptoJS.AES.encrypt(this.info, this.key.trim()).toString()]));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { isDevMode, NgModule } from '@angular/core';
 import { ORIGIN as FUNCTIONS_ORIGIN, NEW_ORIGIN_BEHAVIOR } from '@angular/fire/functions';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { environment } from 'src/environments/environment';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+    HttpClientModule
   ],
   providers: [
     { provide: NEW_ORIGIN_BEHAVIOR, useValue: true },
